b17f92c349c155e97ad2a854b9bbcd468bf829db
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-16 00:52:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const inputToDo = document.getElementById("new-task-description")
+const todoSubmit = document.getElementById("create-task-form")
+const list = document.getElementById("tasks")
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // I just type in the text box
+  // When i click on the submit, add whatever added to Todos
+  
 });
+
+
+todoSubmit.addEventListener('submit', function(event){
+  event.preventDefault();
+
+  // let li = document.createElement("li")
+  // li.innerText = inputToDo.value
+  // list.appendChild(li)
+  
+  list.innerHTML += `<li>${inputToDo.value}</li>`
+  inputToDo.value = null
+})
